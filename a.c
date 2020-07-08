3c78e878b6f10b4798239d8d1dcb22bac795a497
Dale Chen <dalechen0351@google.com>
2020-07-08 11:04:13 +0800

    added swap function

--- a.c
+++ a.c
@@ -4,9 +4,23 @@
 
 
 
-
+void swap(int* a, int* b);
 int main()
 {
 	printf("hello,world\n");
+	int aa = 1;
+	int bb = 2;
+	printf("a = %d, b = %d\n",aa,bb);
+	swap(&aa,&bb);
+	printf("a = %d, b = %d\n",aa,bb);
 
+
+}
+
+void swap(int* a, int* b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
